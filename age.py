e61852e7c5bb18d9eca6fbeec4fc1c5ffed6b93d
DevMjee <dev.mjee2@gmail.com>
2025-08-12 19:19:53 +0100

    Commenting add.py

--- age.py
+++ age.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date
 
-while True:
+while True: # loop to check valid input
     day = input('Please input the date to calculate the age from (dd/mm/yy):\n')
 
     try:
@@ -11,8 +11,8 @@ while True:
             f"[ERROR: {err}] Please input a valid date following the given format.")
 
 
-age = date.today().year - birthday.year
-if date.today().month < birthday.month:
+age = date.today().year - birthday.year # difference in years
+if date.today().month < birthday.month: # subtract 1 year if today month < birthday month
     print(f"This person is {age-1} years old.")
 else:
     print(f"This person is {age} years old.")
